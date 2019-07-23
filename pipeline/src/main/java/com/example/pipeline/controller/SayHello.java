46527defd0682342066ce206b031d7ca7de29a68
E1EA440A4B309571E4E06B9F6999D90C <E1EA440A4B309571E4E06B9F6999D90C@i-search.com.cn>
2019-07-23 14:57:20 +0800

    init

---
+++ pipeline/src/main/java/com/example/pipeline/controller/SayHello.java
@@ -0,0 +1,17 @@
+package com.example.pipeline.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class SayHello {
+
+    @GetMapping("/{name}")
+    public String hello(@PathVariable String name) {
+        return "hello: " + name;
+    }
+
+}
